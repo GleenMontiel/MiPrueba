b78a5dbda749d0f98ea098381b5f4e6efbd4b96a
GleenMontiel <65775735+GleenMontiel@users.noreply.github.com>
2020-05-22 16:53:53 +0200

    Update Hola.java

--- Hola.java
+++ Hola.java
@@ -6,5 +6,6 @@ public class Hola {
     public static void main(String[] args) {
         System.out.println("welcot");
         System.out.println("I have no more branches to commit, said the Ent");
+        System.out.println("modify");
     }
 }
